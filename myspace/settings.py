b557e733f2466df5717adb478cc29b39d45c6444
exaon18 <exaontech@gamil.com>
2026-06-25 12:48:36 +0300

    j

--- myspace/settings.py
+++ myspace/settings.py
@@ -38,7 +38,7 @@ CSRF_TRUSTED_ORIGINS = ['https://myspace-amdb.onrender.com', 'http://myspace-amd
 INSTALLED_APPS = [
     'daphne',
     'django.contrib.admin',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
